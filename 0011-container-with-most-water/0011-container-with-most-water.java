8c5a1f28a604c3c198de917e97ee5cd780033663
AKASHCY046 <144882052+AKASHCY046@users.noreply.github.com>
2025-07-08 13:34:54 +0530

    Time: 6 ms (6.87%), Space: 57.4 MB (86.03%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxArea(int[] arr) {
+        //This follows 2 pointer approch where one is from start and one is frm end
+        int first=0;
+        int last= arr.length-1;
+        int max=0;
+        while(first<last){
+            int d=Math.abs(first-last);
+            int min=Math.min(arr[first],arr[last]);
+            max=Math.max(d*min,max);
+            if(arr[first]<arr[last]){
+                first++;
+            }
+            else{
+                last--;
+            }
+        }
+        return max;
+    }
+}
+// arr=[1,8,6,2,5,4,8,3,7] 
+//      0 1 2 3 4 5 6 7 8 
+
+// arr=[1,1]
+        

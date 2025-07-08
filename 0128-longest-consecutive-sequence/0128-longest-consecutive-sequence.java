a681273a7320fea2838b4f26a3e34ae7b44a4ee9
AKASHCY046 <144882052+AKASHCY046@users.noreply.github.com>
2025-07-08 11:49:09 +0530

    Time: 32 ms (41.82%), Space: 67.7 MB (34.97%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,28 +1,27 @@
 class Solution {
-    public int longestConsecutive(int[] arr) {
-        if(arr.length==0){
-            return 0;
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num); // O(1)
         }
-        int max=0;
-        Arrays.sort(arr);
-        int s=1;
-        for(int i=0;i<arr.length-1;i++){
-            if(arr[i]==arr[i+1]){
-                continue;
-            }
-            if(arr[i]+1==arr[i+1]){
-                s++;
-            }
-            else{
-                max=Math.max(max,s);
-                s=1;
+
+        int maxLength = 0;
+
+        for (int num : set) {  // iterate only unique elements
+            // check only if num is the start of a sequence
+            if (!set.contains(num - 1)) {
+                int current = num;
+                int streak = 1;
+
+                while (set.contains(current + 1)) {
+                    current++;
+                    streak++;
+                }
+
+                maxLength = Math.max(maxLength, streak);
             }
         }
-        
-        return Math.max(max,s);
+
+        return maxLength;
     }
 }
-
-// [0,3,7,2,5,8,4,6,0,1]
-// [0,0,1,2,3,4,5,6,7,8]
-//  1 1 2 3 4 5 6 7 8 9

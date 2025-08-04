c02fd99fd30886bca93be001ae53356465016ab1
AKASHCY046 <144882052+AKASHCY046@users.noreply.github.com>
2025-08-04 14:25:26 +0530

    Time: 1 ms (100%), Space: 42.3 MB (54.92%) - LeetHub

--- 3426-minimum-number-of-chairs-in-a-waiting-room/3426-minimum-number-of-chairs-in-a-waiting-room.java
+++ 3426-minimum-number-of-chairs-in-a-waiting-room/3426-minimum-number-of-chairs-in-a-waiting-room.java
@@ -1,19 +1,15 @@
 class Solution {
     public int minimumChairs(String s) {
-        int e=0;
         int c=0;
-        for(char ch:s.toCharArray()){
-            if(ch=='E'){
-                e++;
-                if(c<e){
-                    c=e;
-                }
-            }
-            else{
-                e--;
+        int m=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='E'){
+                c++;
+                m=Math.max(c,m);
+            }else{
+                c--;
             }
         }
-        return c;
-        
+        return m;
     }
 }

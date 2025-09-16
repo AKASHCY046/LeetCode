757b45c890962f705048460c365ee5f06771f08e
AKASHCY046 <144882052+AKASHCY046@users.noreply.github.com>
2025-09-16 21:02:14 +0530

    Time: 4 ms (27.7%), Space: 42.3 MB (62.91%) - LeetHub

--- 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.java
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.java
@@ -1,10 +1,14 @@
 class Solution {
     public int canBeTypedWords(String text, String str) {
+        HashSet<Character>hs=new HashSet();
+        for(char ch:str.toCharArray()){
+            hs.add(ch);
+        }
         int ans=0;
         for(String x:text.split(" ")){
             boolean is=true;
-            for(char ch:str.toCharArray()){
-                if(x.contains(String.valueOf(ch))){
+            for(char ch:x.toCharArray()){
+                if(hs.contains(ch)){
                     is=false;
                 }
             }

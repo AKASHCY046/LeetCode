1b2024a26ed42c377af2be74cde721d7a04ed214
AKASHCY046 <144882052+AKASHCY046@users.noreply.github.com>
2025-09-21 13:53:59 +0530

    Time: 28 ms (81.2%), Space: 51.4 MB (70%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,35 +1,45 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] arr) {
+        List<List<Integer>>al=new ArrayList<>();
         Arrays.sort(arr);
-        //this follows 2 pointer approch
-        int second = 0;
-        int third = 0;
-        List<List<Integer>> al = new ArrayList<>();
-        for (int first = 0; first < arr.length - 2; first++) {
-            if (first > 0 && arr[first] == arr[first - 1]) {
+        //HINT:this follows 2 pointer approch...
+
+
+
+
+
+        int second=0;
+        int third=0;
+        for(int first=0;first<arr.length-1;first++){
+            
+            if(first>0 && arr[first]==arr[first-1]){
                 continue;
             }
-            second = first + 1;
-            third = arr.length - 1;
-            while (second < third) {
-                if ((arr[first] + arr[second] + arr[third]) > 0) {
+            second=first+1;
+            third=arr.length-1;
+
+            while(second<third){
+                int sum=arr[first]+arr[second]+arr[third];
+                if(sum>0){
                     third--;
-                } else {
-                    if ((arr[first] + arr[second] + arr[third]) < 0) {
+                }
+                else if(sum<0){
+                    second++;
+                }
+                else{
+                    al.add(Arrays.asList(arr[first],arr[second],arr[third]));
+                    second++;
+                    third--;
+                    while(second<third && arr[second]==arr[second-1]){
                         second++;
-                    } else {
-                        al.add(Arrays.asList(arr[first], arr[second], arr[third]));
-                        second++;
+                    }
+
+                    while(second<third && arr[third]==arr[third+1]){
                         third--;
-                        while (second < third && arr[second] == arr[second - 1]) {
-                            second++;
-                        }
-                        while (second < third && arr[third] == arr[third + 1]) {
-                            third--;
-                        }
                     }
                 }
             }
+            
         }
         return al;
     }

c405e9c4e25adad176b42a95e107f9612a2ecba5
AKASHCY046 <144882052+AKASHCY046@users.noreply.github.com>
2025-12-29 22:06:21 +0530

    Time: 143 ms (51.82%), Space: 42.8 MB (73.16%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,36 +1,37 @@
 class Solution {
-     boolean visited[][];
+    boolean[][] visited;
+
     public boolean exist(char[][] board, String word) {
-        
-        int row=board.length;
-        int col=board[0].length;
-        visited=new boolean[row][col];
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(word.charAt(0)==board[i][j] && find(board,i,j,0,word)){
+        int row = board.length, col = board[0].length;
+        visited = new boolean[row][col];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (board[i][j] == word.charAt(0) && find(board, i, j, 0, word)) {
                     return true;
                 }
             }
         }
         return false;
     }
-    public boolean find(char board[][],int i,int j,int index,String word){
-        if(index==word.length()) return true;
 
-        if(i<0 || i>=board.length || j<0 || j>=board[0].length || word.charAt(index)!=board[i][j] || visited[i][j]){
+    public boolean find(char[][] board, int i, int j, int index, String word) {
+        if (index == word.length()) return true;
+
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length
+                || visited[i][j] || board[i][j] != word.charAt(index)) {
             return false;
         }
-        visited[i][j]=true;
-        if(
-            find(board,i+1,j,index+1,word) ||
-            find(board,i-1,j,index+1,word) ||
-            find(board,i,j+1,index+1,word) ||
-            find(board,i,j-1,index+1,word)
-        ){
-            return true;
-        }
-        visited[i][j]=false;
-        return false;
+
+        visited[i][j] = true;
+
+        boolean found =
+                find(board, i + 1, j, index + 1, word) ||
+                find(board, i - 1, j, index + 1, word) ||
+                find(board, i, j + 1, index + 1, word) ||
+                find(board, i, j - 1, index + 1, word);
+
+        visited[i][j] = false;
+        return found;
     }
-    
 }

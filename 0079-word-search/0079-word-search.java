b98f2cba2f8fe6fde861bcebcd5bc57138d5cec7
AKASHCY046 <144882052+AKASHCY046@users.noreply.github.com>
2025-12-29 22:05:47 +0530

    Time: 140 ms (55.74%), Space: 43.1 MB (27.86%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,36 @@
+class Solution {
+     boolean visited[][];
+    public boolean exist(char[][] board, String word) {
+        
+        int row=board.length;
+        int col=board[0].length;
+        visited=new boolean[row][col];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(word.charAt(0)==board[i][j] && find(board,i,j,0,word)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public boolean find(char board[][],int i,int j,int index,String word){
+        if(index==word.length()) return true;
+
+        if(i<0 || i>=board.length || j<0 || j>=board[0].length || word.charAt(index)!=board[i][j] || visited[i][j]){
+            return false;
+        }
+        visited[i][j]=true;
+        if(
+            find(board,i+1,j,index+1,word) ||
+            find(board,i-1,j,index+1,word) ||
+            find(board,i,j+1,index+1,word) ||
+            find(board,i,j-1,index+1,word)
+        ){
+            return true;
+        }
+        visited[i][j]=false;
+        return false;
+    }
+    
+}

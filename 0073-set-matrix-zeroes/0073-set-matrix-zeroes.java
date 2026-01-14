c21779a2f9b8505c79e3908070d54a47d15a902e
AKASHCY046 <144882052+AKASHCY046@users.noreply.github.com>
2026-01-14 20:39:05 +0530

    Time: 3 ms (5%), Space: 47.3 MB (75.65%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,21 +1,20 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
         boolean arr[][]=new boolean[matrix.length][matrix[0].length];
+        System.out.print(matrix.length);
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
                 if(matrix[i][j]==0){
-                   //put true in both the row and col
-                   //for row
-                   for(int x=0;x<matrix[0].length;x++){
-                    arr[i][x]=true;
-                   }
-                   //for col
-                   for(int x=0;x<matrix.length;x++){
-                    arr[x][j]=true;
-                   }
+                    for(int k=0;k<matrix.length;k++){
+                        arr[k][j]=true;
+                    }
+                    for(int k=0;k<matrix[0].length;k++){
+                        arr[i][k]=true;
+                    }
                 }
             }
         }
+
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
                 if(arr[i][j]){
@@ -23,6 +22,14 @@ class Solution {
                 }
             }
         }
-        
     }
 }
+
+/*
+
+00 01 02 03
+10 11 12 13
+20 21 22 23
+30 31 32 33
+
+*/

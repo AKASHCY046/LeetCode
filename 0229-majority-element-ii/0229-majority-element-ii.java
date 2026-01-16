256b180de6c079223bb24e77f004a2b72a0125a3
AKASHCY046 <144882052+AKASHCY046@users.noreply.github.com>
2026-01-16 13:58:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -2,11 +2,9 @@ class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer>li=new ArrayList<>();
         HashMap<Integer,Integer>hs=new HashMap<>();
+        int max=nums.length/3;
         for(int x:nums){
             hs.put(x,hs.getOrDefault(x,0)+1);
-        }
-        int max=nums.length/3;
-        for(int x:hs.keySet()){
             if(hs.get(x)>max){
                 li.add(x);
             }
@@ -14,6 +12,15 @@ class Solution {
                 break;
             }
         }
+        
+        // for(int x:hs.keySet()){
+        //     if(hs.get(x)>max){
+        //         li.add(x);
+        //     }
+        //     if(li.size()==2){
+        //         break;
+        //     }
+        // }
         return li;
     }
 }

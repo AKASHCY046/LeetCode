a31b0cb52efac4a37686b2bffe4e2466b9adf0dc
AKASHCY046 <144882052+AKASHCY046@users.noreply.github.com>
2026-01-18 20:28:59 +0530

    Time: 1 ms (65.04%), Space: 43.5 MB (22.84%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -3,7 +3,6 @@ class Solution {
         Arrays.sort(str);
 
         StringBuilder st=new StringBuilder();
-        System.out.println(str[0]+" "+str[str.length-1]);
         for(int i=0;i<Math.min(str[0].length(),str[str.length-1].length());i++){
             if(str[0].charAt(i)==str[str.length-1].charAt(i)){
                 st.append(str[0].charAt(i));
